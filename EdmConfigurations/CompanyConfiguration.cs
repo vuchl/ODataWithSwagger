c8a69ee3811d089061e80d535bcd8be6f2f2cad9
Sebastian Vogel <s.vogel@sachsenkabel.de>
2019-05-13 08:54:25 +0200

    initial commit

---
+++ EdmConfigurations/CompanyConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ODataWithSwagger.EdmConfigurations
+{
+    public class CompanyConfiguration : IModelConfiguration
+    {
+        public void Apply(ODataModelBuilder builder, ApiVersion apiVersion)
+        {
+            builder.EntitySet<Models.Company>("Companies").EntityType.HasKey(c => c.Id);
+        }
+    }
+}
